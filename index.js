9ac58b52a2906caf68ef3efea4c956f4a78467f0
rajnishxoxo <64491136+rajnishxoxo@users.noreply.github.com>
2023-07-08 15:29:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,39 +1,56 @@
-const listOfAllTheSymbols = "!@#$%^&*()_+~\\`|}{[]:;?><,./-="
-const listOfAllTheNumbers ="1234567890"
-const listOfAllTheLowerCaseLetter= "abcdefghijklmnoqprstuvwxyz"
-const listOfAllTheUpperCaseLetter ="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const listOfAllTheSymbols = "!@#$%^&*()_+~\\`|}{[]:;?><,./-=";
+const listOfAllTheNumbers = "1234567890";
+const listOfAllTheLowerCaseLetter = "abcdefghijklmnoqprstuvwxyz";
+const listOfAllTheUpperCaseLetter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-let passwordString =""
+let passwordString = "";
 
-function genrateSymbols(){
-   let randomSymbol = listOfAllTheSymbols[ Math.floor(Math.random()*listOfAllTheSymbols.length)]
-  
-   passwordString = passwordString+randomSymbol;
-   
-}
+let genratePassword = document.getElementById("btn");
 
-function genrateNumber(){
-    let randomNumber = listOfAllTheNumbers[Math.floor(Math.random()*listOfAllTheNumbers.length)]
-  
-    passwordString = passwordString+randomNumber;
+let includeSymbol = document.getElementById("SymbolCheckBox");
 
-}
+genratePassword.addEventListener("click", function () {
+  console.log(getKey[0])
+});
 
-function genrateUpperCase(){
-    let randomUpperCase = listOfAllTheUpperCaseLetter[Math.floor(Math.random()*listOfAllTheUpperCaseLetter.length)]
-    passwordString = passwordString+randomUpperCase;
-}
+const getKey =[
+    function genrateSymbols() {
+        let randomSymbol =
+          listOfAllTheSymbols[Math.floor(Math.random() * listOfAllTheSymbols.length)];
+      
+        passwordString = passwordString + randomSymbol;
+        console.log(passwordString)
+      } ,
+      
+      function genrateSymbols() {
+        let randomSymbol =
+          listOfAllTheSymbols[Math.floor(Math.random() * listOfAllTheSymbols.length)];
+      
+        passwordString = passwordString + randomSymbol;
+      } ,
+      
+      function genrateNumber() {
+        let randomNumber =
+          listOfAllTheNumbers[Math.floor(Math.random() * listOfAllTheNumbers.length)];
+      
+        passwordString = passwordString + randomNumber;
+      } ,
+       
+      function genrateUpperCase() {
+        let randomUpperCase =
+          listOfAllTheUpperCaseLetter[
+            Math.floor(Math.random() * listOfAllTheUpperCaseLetter.length)
+          ];
+        passwordString = passwordString + randomUpperCase;
+      } ,
+      
+      function genrateLowerCase() {
+        let randomLowerCase =
+          listOfAllTheLowerCaseLetter[
+            Math.floor(Math.random() * listOfAllTheLowerCaseLetter.length)
+          ];
+        passwordString = passwordString + randomLowerCase;
+      }
+      
+]
 
-function genrateLowerCase(){
-    let randomLowerCase = listOfAllTheLowerCaseLetter[Math.floor(Math.random()*listOfAllTheLowerCaseLetter.length)]
-    passwordString = passwordString+randomLowerCase;
-}
-
-for(let i=0 ;i<1;i++){
-    genrateSymbols()
-    genrateNumber()
-    genrateUpperCase()
-    genrateLowerCase()
-}
-
-console.log(passwordString)
