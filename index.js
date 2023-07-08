dcd585f5dce385a5c8f62b8d789d05b9732ab782
rajnishxoxo <64491136+rajnishxoxo@users.noreply.github.com>
2023-07-08 14:18:14 +0530

    Added list of parameter from which password will be Genrated

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-console.log("hello")
+const listOfAllTheSymbols = "!@#$%^&*()_+~\\`|}{[]:;?><,./-="
+const listOfAllTheNumbers ="1234567890"
+const listOfAllTheLowerCaseLetter= "abcdefghijklmnoqprstuvwxyz"
+const listOfAllTheUpperCaseLetter ="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
