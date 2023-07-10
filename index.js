dd3cc57de5d45340cdd63fa18b622f66375b7dcd
rajnishxoxo <64491136+rajnishxoxo@users.noreply.github.com>
2023-07-10 14:39:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,48 +22,6 @@ if(includeSymbol.checked==false&&includeNumber.checked==false&&includeLowercase.
   alert("Select Atleast One Valid Field")
 }
 
-while(passwordLen.length>passwordString.length){
-  let key = getKey[Math.random(Math.floor()*getKey)()]
-  console.log(key)
-  
-}
+
 
 });
-
-const getKey =[
-    function genrateSymbols() {
-        let randomSymbol =
-          listOfAllTheSymbols[Math.floor(Math.random() * listOfAllTheSymbols.length)];
-      
-        passwordString = passwordString + randomSymbol;
-        console.log(passwordString)
-      } ,
-      
-      function genrateNumber() {
-        let randomNumber =
-          listOfAllTheNumbers[Math.floor(Math.random() * listOfAllTheNumbers.length)];
-      
-        passwordString = passwordString + randomNumber;
-        console.log(passwordString)
-      } ,
-       
-      function genrateUpperCase() {
-        let randomUpperCase =
-          listOfAllTheUpperCaseLetter[
-            Math.floor(Math.random() * listOfAllTheUpperCaseLetter.length)
-          ];
-        passwordString = passwordString + randomUpperCase;
-        console.log(passwordString)
-      } ,
-      
-      function genrateLowerCase() {
-        let randomLowerCase =
-          listOfAllTheLowerCaseLetter[
-            Math.floor(Math.random() * listOfAllTheLowerCaseLetter.length)
-          ];
-        passwordString = passwordString + randomLowerCase;
-        console.log(passwordString)
-      }
-      
-]
-
