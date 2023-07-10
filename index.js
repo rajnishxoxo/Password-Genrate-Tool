7f5e25b8f85a808883849dbf27b4e6e6585b30a1
rajnishxoxo <64491136+rajnishxoxo@users.noreply.github.com>
2023-07-10 15:43:00 +0530

    Password genrated and Appended to display Box

--- index.js
+++ index.js
@@ -8,6 +8,44 @@ function getData(list){
   return list[Math.floor(Math.random()*list.length)];
 }
 
+function genratePasswordString(){
+  if(includeSymbol.checked==false&&includeNumber.checked==false&&includeLowercase.checked==false &&includeUpperCase.checked==false){
+    alert("Select Atleast One Valid Field")
+  }
+  
+  if(includeSymbol.checked){
+    passwordString = passwordString+getData(listOfAllTheSymbols)
+  }
+  
+  if(includeNumber.checked){
+    passwordString = passwordString+getData(listOfAllTheNumbers)
+  }
+  
+  if(includeLowercase.checked){
+    passwordString = passwordString+getData(listOfAllTheLowerCaseLetter)
+  }
+  
+  
+  if(includeUpperCase.checked){
+    passwordString = passwordString+getData(listOfAllTheUpperCaseLetter)
+  }
+  
+  if(passwordString.length<passwordLen.value){
+    genratePasswordString();
+  }
+
+  
+
+  passwordBox.value = truncateString(passwordString,passwordLen.value)
+}
+
+function truncateString(string, limit) {
+  if (string.length > limit) {
+    return string.substring(0, limit)
+  } else {
+    return string
+  }
+}
 
 
 let passwordString = "";
@@ -19,37 +57,13 @@ let includeNumber = document.getElementById("numberCheckBox");
 let includeLowercase = document.getElementById("lowerCaseCheckBox");
 let includeUpperCase = document.getElementById("upperCaseCheckBox");
 let passwordLen = document.getElementById('passwordLen');
-
+let passwordBox =document.getElementById('showPassword')
 
 
 
 
 
 genratePassword.addEventListener("click", function () {
-
-if(includeSymbol.checked==false&&includeNumber.checked==false&&includeLowercase.checked==false &&includeUpperCase.checked==false){
-  alert("Select Atleast One Valid Field")
-}
-
-if(includeSymbol.checked){
-  passwordString = passwordString+getData(listOfAllTheSymbols)
-}
-
-if(includeNumber.checked){
-  passwordString = passwordString+getData(listOfAllTheNumbers)
-}
-
-if(includeLowercase.checked){
-  passwordString = passwordString+getData(listOfAllTheLowerCaseLetter)
-}
-
-
-if(includeUpperCase.checked){
-  passwordString = passwordString+getData(listOfAllTheUpperCaseLetter)
-}
-
-
-
-console.log(passwordString)
-
+  genratePasswordString();
+  console.log(passwordString.length)
 });
